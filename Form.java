1039e5bd4c71b7abc21547adb3e0ec19cfe47a8e
owner <owner@owners-MacBook-Pro.local>
2014-05-01 10:31:49 +0300

    release

--- Form.java
+++ Form.java
@@ -13,6 +13,11 @@ import javax.swing.UIManager;
 /**
  * 
  * @author sivan suvari
+ *RSAEncryptor // for big number handling support BigInteger class is used
+ *In this program we are using the rsa algorithm for encoding numbers in
+ *desired bit depth. p and q valus must be prime among them.and e is also
+ *must be prime number. selecting e as a big value will
+ *cause better encryption
  */
 
 public class Form extends javax.swing.JFrame {
--- README.md
+++
@@ -1,7 +0,0 @@
-RSAEncryptor
-============
-
-RSAEncryptor
- // for big number handling support BigInteger class  is used
-In this program we are using the rsa algorithm for encoding numbers in desired bit depth. p and q valus must be prime among them.and e is also must be prime number.
-selecting e as a big value will cause better encryption.
